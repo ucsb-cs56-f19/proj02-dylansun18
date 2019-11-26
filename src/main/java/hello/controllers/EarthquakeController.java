cc96fa2fad35747fbe5312b3045a881d3befc027
Dylan Sun <dylanswafflez@gmail.com>
2019-11-25 18:23:58 -0800

    ds - moved EqSearches.java into searches directory

--- src/main/java/hello/controllers/EarthquakeController.java
+++ src/main/java/hello/controllers/EarthquakeController.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 import com.nimbusds.oauth2.sdk.client.ClientReadRequest;
 import hello.geojson.FeatureCollection;
 import hello.services.EarthquakeQueryService;
-import hello.EqSearch;
+import hello.searches.EqSearch;
 
 
 @Controller
