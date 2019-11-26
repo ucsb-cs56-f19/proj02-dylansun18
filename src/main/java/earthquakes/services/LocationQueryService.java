1a10c8685e993ae5f3108d32f1b5e4115f528e0b
Dylan Sun <dylanswafflez@gmail.com>
2019-11-26 11:39:43 -0800

    ds - added real location query service

--- src/main/java/earthquakes/services/LocationQueryService.java
+++ src/main/java/earthquakes/services/LocationQueryService.java
@@ -1,17 +1,46 @@
 package earthquakes.services;
 
+import java.util.Arrays;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 public class LocationQueryService {
 
     private Logger logger = LoggerFactory.getLogger(LocationQueryService.class);
 
     public String getJSON(String loc) {
-        String fakeJson = "{ \"key\": \"value\" }";
-        String json = fakeJson;
-        logger.info("json=" + json);
-        return json;
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<>("body", headers);
+
+        String url = "https://nominatim.openstreetmap.org/search/" + loc + "?format=json";
+
+        logger.info("url=" + url);
+
+        String retVal="";
+        try {   
+            ResponseEntity<String> re = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+            MediaType contentType = re.getHeaders().getContentType();
+            HttpStatus statusCode = re.getStatusCode();
+            retVal = re.getBody();
+        } catch (HttpClientErrorException e) {
+            retVal = "{\"error\": \"401: Unauthorized\"}";
+        }
+        logger.info("from LocationQueryService.getJSON: " + retVal);
+        return retVal;
     }
 
 } 
